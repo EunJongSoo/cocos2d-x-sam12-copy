d01db6eee21f27399e6be7897b6736d455e14ac9
EunJongSoo <eunjongsoo@gmail.com>
2017-03-20 10:36:45 +0900

    UnitManager 기능 UnitLayer에 합침

--- Classes/UnitLayer.cpp
+++ Classes/UnitLayer.cpp
@@ -1,6 +1,13 @@
 #include "UnitLayer.h"
+#include "PathFindingManager.h"
+#include "UnitSprite.h"
 
-CUnitLayer::CUnitLayer() {}
+using namespace cocos2d;
+using std::vector;
+
+CUnitLayer::CUnitLayer() {
+	path_manager = new PathFindingManager();
+}
 
 CUnitLayer::~CUnitLayer() {}
 
@@ -8,4 +15,56 @@ bool CUnitLayer::init()
 {
 	assert(Layer::init());
 	return true;
+}
+
+void CUnitLayer::create_unit() {
+	CUnitSprite* sprite = CUnitSprite::create();
+	sprite->setPosition(500, 300);
+	sprite->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+	unit_vector.push_back(sprite);
+	this->addChild(sprite);
+}
+
+void CUnitLayer::clear_selete_unit() {
+	int size = selete_unit_vector.size();
+	for (CUnitSprite* sprite : selete_unit_vector) {
+		sprite->stop_action_animate();
+	}
+	selete_unit_vector.clear();
+}
+
+const bool CUnitLayer::selete_unit(const cocos2d::Vec2& _vec2) {
+	for (CUnitSprite* sprite : unit_vector) {
+		if (sprite->getBoundingBox().containsPoint(_vec2)) {
+			selete_unit_vector.push_back(sprite);
+			return true;
+		}
+	}
+	return false;
+}
+
+void CUnitLayer::blinking_selete_unit() {
+	for (CUnitSprite* sprite : selete_unit_vector) {
+		sprite->run_action_animate(CUnitSprite::action_list::blinking);
+	}
+}
+
+// 임시 작성
+void CUnitLayer::move_unit(const Vec2& _vec2) {
+	vector<Vec2*> vector;
+	for (CUnitSprite* sprite : selete_unit_vector) {
+
+		int start, end;
+		start = clock();
+		path_manager->finding_path(sprite->getPosition().x, sprite->getPosition().y, _vec2.x, _vec2.y);
+		end = clock();
+		CCLOG("finding_path end : %d", end - start);
+
+		std::vector<PathFindingManager::vec2*>& tmpvector = path_manager->goal_path;
+		for (PathFindingManager::vec2* _vec2 : tmpvector) {
+			vector.push_back(new Vec2(_vec2->x, _vec2->y));
+		}
+		path_manager->goal_path_clear();
+		sprite->run_action_move_unit(vector);
+	}
 }
--- Classes/UnitLayer.h
+++ Classes/UnitLayer.h
@@ -1,15 +1,27 @@
 #ifndef UNIT_LAYER_H_
 #define UNIT_LAYER_H_
 
+#include <vector>
 #include "cocos2d.h"
 
+class CUnitSprite;
+class PathFindingManager;
 class CUnitLayer : public cocos2d::Layer {
 public:
 	CUnitLayer();
 	~CUnitLayer();
 	virtual bool init();
 	CREATE_FUNC(CUnitLayer);
+
+	void create_unit();
+	void clear_selete_unit();
+	const bool selete_unit(const cocos2d::Vec2& _vec2);
+	void blinking_selete_unit();
+	void move_unit(const cocos2d::Vec2& _vec2);
 private:
+	std::vector<CUnitSprite*> unit_vector;
+	std::vector<CUnitSprite*> selete_unit_vector;
+	PathFindingManager* path_manager;
 	
 };
 #endif
